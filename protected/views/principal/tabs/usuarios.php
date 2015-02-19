434b7d881f2e2307f6a66899e8f8bb20493d77b1
shizukaro <karo_diaz27@hotmail.com>
2015-02-19 13:21:11 -0600

    actualiza

--- protected/views/principal/tabs/usuarios.php
+++ protected/views/principal/tabs/usuarios.php
@@ -1,1 +1,19 @@
-<H1 >USUARIOS SPACE</H1>
+<?php
+/* @var $this PrincipalController */
+/* @var $model Usuario */
+?>
+
+<div class="form">
+	
+	Usuario:
+	<input class="cuadro-text"></input>
+	<div class="row buttons">
+		<div class="btn btn-default" >Modificar</div>
+		<div class="btn btn-default" >Agregar</div>
+		<div class="btn btn-default" >Eliminar</div>
+		<div class="btn btn-default" >Aceptar</div>
+	</div>
+	
+	
+</div>
+
